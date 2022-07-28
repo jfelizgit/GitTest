39d53c2bfd402977fe569fe2e0106c5da853bd14
jfelizgit <109927299+jfelizgit@users.noreply.github.com>
2022-07-28 08:58:54 -0400

    button added

--- GitTest/Form1.Designer.cs
+++ GitTest/Form1.Designer.cs
@@ -30,6 +30,7 @@ namespace GitTest
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -43,11 +44,21 @@ namespace GitTest
             this.label1.Text = "Version 1.1";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(143, 168);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Version";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(377, 264);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -59,6 +70,7 @@ namespace GitTest
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
